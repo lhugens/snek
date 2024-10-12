a7f1714de20c5587c3a57eb8535a5cfd42622a16
Leonardo Hugens <leonardohugens@protonmail.com>
2024-10-12 17:22:38 +0100

    Snake created and moving

--- Snake/src/io/codeforall/fanstatics/Snake.java
+++ Snake/src/io/codeforall/fanstatics/Snake.java
@@ -11,24 +11,59 @@ import java.awt.*;
 
 public class Snake implements KeyboardHandler {
     private Rectangle head;
+    private int cellSize;
     private Rectangle[] body;
-    private int xCoordHead;
-    private int yCoordHead;
+    private int size;
+    private int[] headCoords;
+    private int[] oldCoords;
     private int speed;
     public Keyboard keyboard;
 
     public Snake() {
         this.speed = 5;
+        this.size = 10;
+        this.cellSize = 10;
+        this.body = new Rectangle[this.size];
 
-        int xCoordHead = (int) (Math.random() * 200);
-        int yCoordHead = (int) (Math.random() * 200);
+        this.headCoords = new int[2];
+        this.headCoords[0] = (int) (Math.random() * 200);
+        this.headCoords[1] = (int) (Math.random() * 200);
+        this.oldCoords = new int[2];
+        this.oldCoords[0] = this.headCoords[0];
+        this.oldCoords[1] = this.headCoords[1];
 
-        this.head = new Rectangle(xCoordHead, yCoordHead, 10, 10);
+        this.head = new Rectangle(this.headCoords[0], this.headCoords[1], this.cellSize, this.cellSize);
         this.head.setColor(Color.GREEN);
         this.head.fill();
 
+        this.body[0] = this.head;
+
+        for (int i = 1; i < size; i++) {
+            this.body[i] = new Rectangle(this.headCoords[0], this.headCoords[1] - this.cellSize * i, this.cellSize, this.cellSize);
+            this.body[i].setColor(Color.GREEN);
+            this.body[i].fill();
+        }
+
         initKeyboard();
     }
+
+    public void moveBody(int[] oldCoords) {
+        int[] previous = new int[2];
+        previous[0] = oldCoords[0];
+        previous[1] = oldCoords[1];
+
+        for (int i = 1; i < this.body.length; i++) {
+            int[] temp = new int[2];
+            temp[0] = this.body[i].getX();
+            temp[1] = this.body[i].getY();
+
+            this.body[i].translate(previous[0] - temp[0], previous[1] - temp[1]);
+
+            previous[0] = temp[0];
+            previous[1] = temp[1];
+        }
+    }
+
 
     private void initKeyboard() {
         this.keyboard = new Keyboard(this);
@@ -60,20 +95,27 @@ public class Snake implements KeyboardHandler {
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
+        this.oldCoords[0] = this.headCoords[0];
+        this.oldCoords[1] = this.headCoords[1];
         switch (keyboardEvent.getKey()) {
             case KeyboardEvent.KEY_RIGHT:
                 this.head.translate(this.speed, 0);
+                headCoords[0] += this.speed;
                 break;
             case KeyboardEvent.KEY_LEFT:
                 this.head.translate(-this.speed, 0);
+                headCoords[0] -= this.speed;
                 break;
             case KeyboardEvent.KEY_UP:
                 this.head.translate(0, -this.speed);
+                headCoords[1] -= this.speed;
                 break;
             case KeyboardEvent.KEY_DOWN:
                 this.head.translate(0, this.speed);
+                headCoords[1] += this.speed;
                 break;
         }
+        this.moveBody(this.oldCoords);
     }
 
     @Override
